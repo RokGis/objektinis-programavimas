7faa33aac569463ce486727d40318ab2bef6beb2
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-02-13 21:36:19 +0200

    update

--- pirmauzd C.cpp
+++ pirmauzd C.cpp
@@ -19,20 +19,6 @@ int main()
     int m = 0, sum = 0;
     double vid;
 
-    // cout << "Iveskite studentu skaiciu (naturalusis skaicius): ";
-    // while (!(cin >> m) || m <= 0) {
-    //     cout << "Netinkamas ivesties formatas. Iveskite naturaluji skaiciu: " << endl;
-    //     cin.clear();
-    //     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    // }
-
-    // cout << "Iveskite studentu namu darbu pazymiu skaiciu (naturalusis skaicius): ";
-    // while (!(cin >> n) || n <= 0) {
-    //     cout << "Netinkamas ivesties formatas. Iveskite naturaluji skaiciu: " << endl;
-    //     cin.clear();
-    //     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    // }
-
     char budas;
     cout << "Pasirinkite galutinio balo apskaiciavimo buda (vidurkis (v) ar mediana (m)): ";
     cin >> budas;

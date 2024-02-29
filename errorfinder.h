f88ed03debdf7639411194b70dd1081b39ef016b
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-02-29 21:17:35 +0200

    update

---
+++ errorfinder.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int ivedbudpatikra(int ivedbudas);
+char budaspatikra(char budas);
+char dskaitpatikra(char duomskait);
+int studskpatikra(int m);
+char isvedbudpatikra(char isvedbud);

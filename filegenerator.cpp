a5ba27b5afccaed39c4e3842b010ad78cc358cc6
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-03-06 20:03:12 +0200

    update

--- filegenerator.cpp
+++ filegenerator.cpp
@@ -9,10 +9,18 @@ int failugeneravimas ()
     cin >> n;
     cout << "Iveskite namu darbu pazymiu skaiciu: ";
     cin >> m;
+
+    out << setw(25) << left << "Vardas" << setw(25) << left << "Pavarde";
+    for (int i = 0; i < m; i++)
+    {
+        out << setw(8) << right << "ND" + to_string(i + 1);
+    }
+    out << setw(8) << right << "Egz." << endl;
+
     for (int i = 0; i < n; i++)
     {
         out << setw(25) << left << "Vardas" + to_string(i + 1) << setw(25) << left << "Pavarde" + to_string(i + 1);
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < m + 1; j++)
         {
             out << right << setw(8) << rand() % 11;
         }

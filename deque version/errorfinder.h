33c4929ca7f64ff75b36358fb2a749e2c506f3d4
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-03-18 10:27:23 +0200

    update

---
+++ deque version/errorfinder.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int ivedbudpatikra();
+char budaspatikra();
+char dskaitpatikra();
+int studskpatikra();
+char isvedbudpatikra();
+int erezpatikra();
+char rikbudpatikra();
+int pazymiopatikra();
+char skirstymopatikra();
+char fgeneravimopatikra();
